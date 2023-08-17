0856de5ef2ba994eba1b7380e31e5235a4ebb813
faust <faust3719@gmail.com>
2023-08-17 16:51:10 +0700

    default

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 
 yum install -y php82 php82-php-fpm php82-php-cli php82-php-mysqlnd php82-php-gd php82-php-ldap php82-php-odbc php82-php-pdo php82-php-pear php82-php-xml php82-php-xmlrpc php82-php-mbstring php82-php-snmp php82-php-soap php82-php-zip php82-php-opcache nginx git mc nano
 
-systemctl start nginx
-systemctl start --now php82-php-fpm
 systemctl enable nginx
 systemctl enable --now php82-php-fpm
 
@@ -15,6 +13,11 @@ cd appHusky
 unalias cp
 cp -r core/default/* /
 
+adduser appHusky
+gpasswd -a appHusky wheel
+gpasswd -a appHusky root
+chmod -R 666 /var/appHusky
+
 systemctl restart nginx
 systemctl restart php82-php-fpm
-(/var/appHusky/log/system/php/error.log
+
