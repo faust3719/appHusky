d350ae699f25e1a05bda06ef7a21ca350e87054e
faust <faust3719@gmail.com>
2023-08-17 17:24:40 +0700

    default

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
+setenforce 0
+sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+
 yum install -y epel-release
 yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 
-yum install -y php82 php82-php-fpm php82-php-cli php82-php-mysqlnd php82-php-gd php82-php-ldap php82-php-odbc php82-php-pdo php82-php-pear php82-php-xml php82-php-xmlrpc php82-php-mbstring php82-php-snmp php82-php-soap php82-php-zip php82-php-opcache nginx git mc nano
+yum install -y php82 php82-php-fpm php82-php-cli php82-php-mysqlnd php82-php-gd php82-php-ldap php82-php-odbc php82-php-pdo php82-php-pear php82-php-xml php82-php-xmlrpc php82-php-mbstring php82-php-snmp php82-php-soap php82-php-zip php82-php-opcache nginx git mc nano iptables
 
 systemctl enable nginx
 systemctl enable --now php82-php-fpm
@@ -18,7 +21,9 @@ gpasswd -a appHusky wheel
 gpasswd -a appHusky root
 chmod -R 666 /var/appHusky
 
-mkdir /run/php
+mkdir -p /run/php
+mkdir -p /var/appHusky/log/nginx/default
+mkdir -p /var/appHusky/log/system/nginx
 
 systemctl restart nginx
 systemctl restart php82-php-fpm
