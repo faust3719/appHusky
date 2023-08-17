931eeee402a09bec504e5abbaf3f890495068d18
faust <faust3719@gmail.com>
2023-08-17 14:20:21 +0700

    init

---
+++ install.sh
@@ -0,0 +1,8 @@
+yum install -y epel-release
+yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
+yum install -y php82 php82-php-fpm php82-php-cli php82-php-mysqlnd php82-php-gd php82-php-ldap php82-php-odbc php82-php-pdo php82-php-pear php82-php-xml php82-php-xmlrpc php82-php-mbstring php82-php-snmp php82-php-soap php82-php-zip php82-php-opcache nginx
+
+cd /etc/
+
+
